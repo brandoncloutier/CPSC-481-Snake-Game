0e6efc7a59de553a5aa2fe0be8f8f9b917ec8b58
bcloutier412 <brandoncloutier412@gmail.com>
2026-02-25 16:29:29 -0800

    abstracted out the collision logic to separate per object methods

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ WHITE = pygame.Color(255, 255, 255)
 GREEN = pygame.Color(0, 255, 0)
 RED = pygame.Color(255, 0, 0)
 
-score = [0, 0]
-
 pygame.init()
 pygame.font.init()
 pygame.display.set_caption("AI Snake Project")
@@ -37,28 +35,30 @@ fps = pygame.time.Clock()
 
 class SnakePlayer:
     def __init__(
-        self, screen_width, screen_height, offset=0, TILE_SIZE=20, FRUIT_SIZE=20
+        self, screen_width, screen_height, offset=0, tile_size=TILE_SIZE, fruit_size=FRUIT_SIZE
     ):
-        self.SNAKE_SPEED = 20
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.offset = offset
 
-        self.TILE_SIZE = TILE_SIZE
-        self.FRUIT_SIZE = FRUIT_SIZE
+        self.tile_size = tile_size
+        self.fruit_size = fruit_size
+        self.grid_width = self.screen_width // self.tile_size
+        self.grid_height = self.screen_height // self.tile_size
 
-        self.running = True
         self.score = 0
 
         self.direction = "right"
         self.turn_to = self.direction
 
-        self.snake_pos = [20 + self.offset, 40]
+        start_x, start_y = grid_to_pixel(1, 2, self.tile_size)
+        self.snake_pos = [start_x + self.offset, start_y]
         self.snake_body = [
-            [20 + self.offset, 40],
-            [20 - self.SNAKE_SPEED + self.offset, 40],
-            [20 - (self.SNAKE_SPEED * 2) + self.offset, 40],
-            [20 - (self.SNAKE_SPEED * 3) + self.offset, 40],
+            # Initial Body
+            [start_x + self.offset, start_y],
+            [start_x - self.tile_size + self.offset, start_y],
+            [start_x - (self.tile_size * 2) + self.offset, start_y],
+            [start_x - (self.tile_size * 3) + self.offset, start_y],
         ]
 
         self.fruit_pos = [0, 0]
@@ -74,90 +74,89 @@ class SnakePlayer:
         if turn_to == "down" and self.direction != "up":
             self.direction = "down"
 
-    def game_tick(self):
+    def update(self):
         if self.direction == "up":
-            self.snake_pos[1] -= TILE_SIZE
+            self.snake_pos[1] -= self.tile_size
         if self.direction == "down":
-            self.snake_pos[1] += TILE_SIZE
+            self.snake_pos[1] += self.tile_size
         if self.direction == "right":
-            self.snake_pos[0] += TILE_SIZE
+            self.snake_pos[0] += self.tile_size
         if self.direction == "left":
-            self.snake_pos[0] -= TILE_SIZE
+            self.snake_pos[0] -= self.tile_size
 
         self.snake_body.insert(0, list(self.snake_pos))
-        if (
-            self.snake_pos[0] == self.fruit_pos[0]
-            and self.snake_pos[1] == self.fruit_pos[1]
-        ):
+        if (self.snake_pos[0] == self.fruit_pos[0] and self.snake_pos[1] == self.fruit_pos[1]):
             self.score += 10
             self.fruit_spawned = False
         else:
             self.snake_body.pop()
 
         if not self.fruit_spawned:
+            grid_x = random.randrange(0, self.grid_width)
+            grid_y = random.randrange(0, self.grid_height)
             self.fruit_pos = [
-                (random.randrange(1, self.screen_width // 20) * 20 + self.offset),
-                (random.randrange(1, self.screen_height // 20) * 20),
+                grid_x * self.tile_size + self.offset,
+                grid_y * self.tile_size,
             ]
 
         self.fruit_spawned = True
 
-        return self.snake_pos, self.snake_body, self.score, self.fruit_pos
+    def is_self_collision(self):
+        for tile in self.snake_body[1:]:
+            if self.snake_pos[0] == tile[0] and self.snake_pos[1] == tile[1]:
+                return True
+        return False
+
+    def is_out_of_bounds(self, min_x, max_x, min_y, max_y):
+        if (self.snake_pos[0] < min_x or self.snake_pos[0] >= max_x or self.snake_pos[1] < min_y or self.snake_pos[1] >= max_y):
+            return True
+        return False
+
+    def render(self, surface, color):
+        for tile in self.snake_body:
+            pygame.draw.rect(surface, color, pygame.Rect(tile[0], tile[1], self.tile_size, self.tile_size))
+
+        pygame.draw.rect(surface, RED, pygame.Rect(self.fruit_pos[0], self.fruit_pos[1], self.fruit_size, self.fruit_size),)
 
 
-def pixel_to_grid(pixel_x, pixel_y, grid_size=20):
+def pixel_to_grid(pixel_x, pixel_y, grid_size=TILE_SIZE):
     return pixel_x // grid_size, pixel_y // grid_size
 
 
-def grid_to_pixel(grid_x, grid_y, grid_size=20):
+def grid_to_pixel(grid_x, grid_y, grid_size=TILE_SIZE):
     return grid_x * grid_size, grid_y * grid_size
 
 
-def render_score(player_id):
+def render_score(surface, score_value, player_id, split_screen_width, mid_bar_width, color):
     font = pygame.font.SysFont("arial", 30)
-    score_surface = font.render("Score: " + str(score[player_id]), True, WHITE)
+    score_surface = font.render("Score: " + str(score_value), True, color)
 
     score_rect = score_surface.get_rect()
-    score_rect.topleft = (10 + player_id * (SPLIT_SCREEN_WIDTH + MID_BAR_WIDTH), 10)
+    score_rect.topleft = (10 + player_id * (split_screen_width + mid_bar_width), 10)
 
-    window.blit(score_surface, score_rect)
+    surface.blit(score_surface, score_rect)
 
 
-def game_over(player_id):
+def game_over(surface, score_value, player_id, split_screen_width, split_screen_height, color):
     font = pygame.font.SysFont("arial", 30)
     line_height = 40
-    game_over_surface = font.render("Game Over!", True, WHITE)
-    text_surface = font.render("Score: " + str(score[player_id]), True, WHITE)
+    game_over_surface = font.render("Game Over!", True, color)
+    text_surface = font.render("Score: " + str(score_value), True, color)
 
     game_rect = game_over_surface.get_rect()
-    game_rect.midtop = (
-        SPLIT_SCREEN_WIDTH / 2 + player_id * SPLIT_SCREEN_WIDTH,
-        SPLIT_SCREEN_HEIGHT / 2 - line_height / 2,
-    )
+    game_rect.midtop = ( split_screen_width / 2 + player_id * split_screen_width, split_screen_height / 2 - line_height / 2)
 
     text_rect = text_surface.get_rect()
-    text_rect.midtop = (
-        SPLIT_SCREEN_WIDTH / 2 + player_id * SPLIT_SCREEN_WIDTH,
-        SPLIT_SCREEN_HEIGHT / 2 + line_height / 2,
-    )
+    text_rect.midtop = (split_screen_width / 2 + player_id * split_screen_width, split_screen_height / 2 + line_height / 2)
 
-    window.blit(game_over_surface, game_rect)
-    window.blit(text_surface, text_rect)
+    surface.blit(game_over_surface, game_rect)
+    surface.blit(text_surface, text_rect)
     pygame.display.flip()
-
-
-def spawn_fruit(fruit_pos):
-    pygame.draw.rect(window, RED, pygame.Rect(fruit_pos[0], fruit_pos[1], 20, 20))
-
 
 if __name__ == "__main__":
     # Initializing Player and AI game objects
     player = SnakePlayer(SPLIT_SCREEN_WIDTH, SPLIT_SCREEN_HEIGHT)
-    ai = SnakePlayer(
-        SPLIT_SCREEN_WIDTH,
-        SPLIT_SCREEN_HEIGHT,
-        offset=SPLIT_SCREEN_WIDTH + MID_BAR_WIDTH,
-    )
+    ai = SnakePlayer(SPLIT_SCREEN_WIDTH, SPLIT_SCREEN_HEIGHT, offset=SPLIT_SCREEN_WIDTH + MID_BAR_WIDTH)
 
     # ================================================================================================
     # MAIN GAME LOOP
@@ -201,75 +200,49 @@ if __name__ == "__main__":
         # This will be called every game tick.
 
         # Fetching game state of player
-        player_pos, player_body, score[0], player_fruit = player.game_tick()
+        player.update()
 
         # Fetching game state of AI
-        ai_pos, ai_body, score[1], ai_fruit = ai.game_tick()
+        ai.update()
 
         # =================================
         # Checking for game over conditions
         # =================================
 
         # Checks for Player self collision
-        for tile in player_body[1:]:
-            if player_pos[0] == tile[0] and player_pos[1] == tile[1]:
-                player_id = 0
-                running = False
-
-        # Checks for AI self collision
-        for tile in ai_body[1:]:
-            if ai_pos[0] == tile[0] and ai_pos[1] == tile[1]:
-                player_id = 1
-                running = False
-
-        # Checks for Player out of bounds
-        if player_pos[0] >= SPLIT_SCREEN_WIDTH or player_pos[0] < 0:
-            print("player went outside of play zone1")
+        if player.is_self_collision():
             player_id = 0
             running = False
-        if player_pos[1] >= SPLIT_SCREEN_HEIGHT or player_pos[1] < 0:
+
+        # Checks for AI self collision
+        if ai.is_self_collision():
+            player_id = 1
+            running = False
+
+        # Checks for Player out of bounds
+        if player.is_out_of_bounds(0, SPLIT_SCREEN_WIDTH, 0, SPLIT_SCREEN_HEIGHT):
             print("player went outside of play zone1")
             player_id = 0
             running = False
 
         # Checks for AI out of bounds
-        if ai_pos[0] > WINDOW_WIDTH or ai_pos[0] < SPLIT_SCREEN_WIDTH:
+        if ai.is_out_of_bounds(SPLIT_SCREEN_WIDTH, WINDOW_WIDTH, 0, WINDOW_HEIGHT):
             print("ai went outside of play zone")
-            player_id = 1
-            running = False
-        if ai_pos[1] > WINDOW_HEIGHT or ai_pos[1] < 0:
-            print("ai went outside of play zone1")
             player_id = 1
             running = False
 
         # ==================
         # Drawing the Canvas
         # ==================
-        window.fill(
-            BLACK
-        )  # make sure that any drawing code is after this point, or else it doesn't show up.
+        window.fill(BLACK)  # make sure that any drawing code is after this point, or else it doesn't show up.
 
-        pygame.draw.rect(
-            window,
-            WHITE,
-            pygame.Rect(SPLIT_SCREEN_WIDTH, 0, MID_BAR_WIDTH, WINDOW_HEIGHT),
-        )
+        pygame.draw.rect(window, WHITE, pygame.Rect(SPLIT_SCREEN_WIDTH, 0, MID_BAR_WIDTH, WINDOW_HEIGHT))
 
-        render_score(0)
-        render_score(1)
+        render_score(window, player.score, 0, SPLIT_SCREEN_WIDTH, MID_BAR_WIDTH, WHITE)
+        render_score(window, ai.score, 1, SPLIT_SCREEN_WIDTH, MID_BAR_WIDTH, WHITE)
 
-        for tile in player_body:
-            pygame.draw.rect(
-                window, WHITE, pygame.Rect(tile[0], tile[1], TILE_SIZE, TILE_SIZE)
-            )
-
-        for tile in ai_body:
-            pygame.draw.rect(
-                window, WHITE, pygame.Rect(tile[0], tile[1], TILE_SIZE, TILE_SIZE)
-            )
-
-        spawn_fruit(player_fruit)
-        spawn_fruit(ai_fruit)
+        player.render(window, WHITE)
+        ai.render(window, WHITE)
 
         pygame.display.update()
 
@@ -279,5 +252,6 @@ if __name__ == "__main__":
     # END GAME Process
     # ================================================================================================
     print(player_id)
-    game_over(player_id)
+    final_score = player.score if player_id == 0 else ai.score
+    game_over(window, final_score, player_id, SPLIT_SCREEN_WIDTH, SPLIT_SCREEN_HEIGHT, WHITE)
     time.sleep(2)
